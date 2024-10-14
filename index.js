ac3afff4cf09d2bdc5796700246051c9940d1ffd
XgHost say <146603842+Daemonsdev@users.noreply.github.com>
2024-10-14 17:41:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,8 @@ const sendContactMessage = (senderId) => {
   });
 };
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   loadMenuCommands();
 });
